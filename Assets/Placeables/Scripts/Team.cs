869af6d74945d5c2477a4a6a6ac1648b593b8bc6
alexc96161 <59664193+alexc96161@users.noreply.github.com>
2020-01-08 15:08:40 -0800

    Add device check system
    -Add: System to check if the team script is on a device or material.

--- Assets/Placeables/Scripts/Team.cs
+++ Assets/Placeables/Scripts/Team.cs
@@ -5,4 +5,28 @@ using UnityEngine;
 public class Team : MonoBehaviour
 {
     public int team;
+    private bool IsDevice;
+
+    
+    private void Start()
+    {
+        CheckIfIsDevice();
+    }
+
+    private void CheckIfIsDevice ()
+    {
+        if (GetComponent<PlaceablesScript>())
+        {
+            IsDevice = true;
+        }
+        else if (GetComponent<PlatformStrut>())
+        {
+            IsDevice = false;
+        }
+        else
+        {
+            Debug.LogError("Error: Not Device or material - Team Script on gameobject: " + gameObject);
+        }
+    }
+
 }
