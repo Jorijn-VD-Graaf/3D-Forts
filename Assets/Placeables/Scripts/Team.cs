bc8e071456e42c0a579b7da1e111b78ff15dd0ac
alexc96161 <59664193+alexc96161@users.noreply.github.com>
2020-01-10 07:28:30 -0800

    Set device teams to thier material.
    Add: A function which gets the material a device is on a sets that devices team to that materials team.

--- Assets/Placeables/Scripts/Team.cs
+++ Assets/Placeables/Scripts/Team.cs
@@ -8,11 +8,16 @@ public class Team : MonoBehaviour
     private bool IsDevice;
 
     
-    private void Start()
+    void Start()
     {
         CheckIfIsDevice();
+        if (IsDevice)
+        {
+            SetTeamToMaterial();
+        }
     }
 
+    // Check if it is a device, material or neather.
     private void CheckIfIsDevice ()
     {
         if (GetComponent<PlaceablesScript>())
@@ -26,6 +31,20 @@ public class Team : MonoBehaviour
         else
         {
             Debug.LogError("Error: Not Device or material - Team Script on gameobject: " + gameObject);
+        }
+    }
+
+    private void SetTeamToMaterial()
+    {
+        var JointFixed = gameObject.GetComponent<FixedJoint>();
+        if (JointFixed != null)
+        {
+            var RigBodyOther = JointFixed.connectedBody;
+            var Material = RigBodyOther.gameObject;
+            if (Material.GetComponent<Team>() != null)
+            {
+                team = Material.GetComponent<Team>().team;
+            }
         }
     }
 
